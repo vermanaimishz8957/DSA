7ef1b81dca3cb95f6ba37ae7375f910b4088002c
Naimish Kumar Verma <153951460+vermanaimishz8957@users.noreply.github.com>
2026-04-05 11:49:20 +0530

    Time: 256 ms (66.82%) | Memory: 96.8 MB (12.51%) - LeetSync

--- 2731-memoize/memoize.js
+++ 2731-memoize/memoize.js
@@ -4,16 +4,24 @@
  */
 function memoize(fn) {
     const cache = new Map();
+    let callCount = 0;
 
-    return function(...args) {
+    function memoized(...args) {
         const key = JSON.stringify(args);
 
         if (cache.has(key)) {
             return cache.get(key);
         }
 
+        callCount++; // increment only when actual function is called
         const result = fn(...args);
         cache.set(key, result);
         return result;
     }
+
+    memoized.getCallCount = function () {
+        return callCount;
+    };
+
+    return memoized;
 }
