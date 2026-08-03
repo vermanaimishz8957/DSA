220ad112f24aa2cbaed1ed17f8d737fdb113a52b
Naimish Kumar Verma <153951460+vermanaimishz8957@users.noreply.github.com>
2026-08-03 21:17:52 +0530

    Time: 10 ms (83.24%) | Memory: 85.1 MB (88.92%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = 0;
+            dp[i] = Integer.MIN_VALUE;
+
+            for (int j = 0; j < 3 && i + j < n; j++) {
+                sum += stoneValue[i + j];
+                dp[i] = Math.max(dp[i], sum - dp[i + j + 1]);
+            }
+        }
+
+        if (dp[0] > 0) {
+            return "Alice";
+        } else if (dp[0] < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+}
